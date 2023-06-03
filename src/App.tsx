2b523d04fc772aee0f2cf2815d3bb9ad0e7cf6df
Fernando de Bona <fbonareis@gmail.com>
2023-06-03 15:38:55 -0300

    feat: add base styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
+import { ThemeProvider } from 'styled-components'
+
+import { GlobalStyles } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
+
 function App() {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <h1>Heey</h1>
-    </>
+
+      <GlobalStyles />
+    </ThemeProvider>
   )
 }
 
